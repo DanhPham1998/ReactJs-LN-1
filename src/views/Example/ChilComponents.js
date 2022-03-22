3be31d175901ee86ff75953b0cb78b8cf2dc77b8
DanhPham1998 <danhkid000@gmail.com>
2022-03-22 10:28:21 +0700

    up

---
+++ src/views/Example/ChilComponents.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// class ChilComponents extends React.Component {
+//     handleClickBtn = () => {
+//         alert("Click");
+//     };
+//     render() {
+//         let { name, age, address, jobs } = this.props;
+//         return (
+//             <>
+//                 <div className="Jobs-List">
+//                     {jobs.map((item, index) => {
+//                         return (
+//                             <div key={item.id}>
+//                                 {item.nameJob}-{item.salary}
+//                             </div>
+//                         );
+//                     })}
+//                 </div>
+//                 Tao ten la {name} va tao {age} tuoi, tao o {address}
+//             </>
+//         );
+//     }
+// }
+const ChilComponents = () => {
+    return <div>Hello</div>;
+};
+export default ChilComponents;
--- src/views/Example/Mycomponents.js
+++ src/views/Example/Mycomponents.js
@@ -1,16 +1,49 @@
 import React from "react";
+import ChilComponents from "./ChilComponents";
 
-const user = {
-    name: "Khang",
-    age: 25,
-};
 class Mycomponents extends React.Component {
+    state = {
+        name: "",
+        age: 24,
+        arrJobs: [
+            { id: 1, nameJob: "IT", salary: "200 $" },
+            { id: 2, nameJob: "DI~", salary: "400 $" },
+            { id: 3, nameJob: "Desiner", salary: "600 $" },
+        ],
+    };
+    handleClickBtn = () => {
+        alert("Click");
+    };
+    handleChangeName = (event) => {
+        this.setState({
+            name: event.target.value,
+        });
+    };
     render() {
+        console.log("Check State", this.state);
         return (
             <>
-                <div>
-                    Hello Viet nam {user.name} va {user.age}
+                <div className="input-1">
+                    <label>Name</label>
+                    <br />
+                    <input
+                        type="text"
+                        value={this.state.name}
+                        onChange={(event) => this.handleChangeName(event)}
+                    />
                 </div>
+                <div>
+                    <button onClick={() => this.handleClickBtn()}>
+                        Click One
+                    </button>
+                </div>
+
+                <ChilComponents
+                    name={"danh"}
+                    age={25}
+                    address={"Nha Trang"}
+                    jobs={this.state.arrJobs}
+                />
             </>
         );
     }
